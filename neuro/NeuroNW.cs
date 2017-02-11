9be1e460e6614d11ceb6a2632d64bef9161d0d54
SteamDiver <steamdiver@yandex.ru>
2017-02-11 21:36:11 +0000

    initial

---
+++ neuro/NeuroNW.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace neuro
+{
+	public class NeuroNW
+	{
+		neuron[][] neuronlist;
+		public NeuroNW(int[] network,int inputs)
+		{
+			//построение листа нейронов
+			neuronlist = new neuron[network.Length][];
+			for (int i = 0; i < network.Length; i++)
+			{
+				neuronlist[i] = new neuron[network[i]];
+			}
+			for (int i = 0; i < inputs; i++)
+			{
+				neuronlist[0][i] = new neuron(inputs, new double[] { 1, 1 });
+			}
+			for (int i = 1; i < network.Length;i++)
+				for (int k = 0; k < network[i];k++)
+				{
+					neuronlist[i][k] = new neuron(network[i-1], new double[] {1,-100 });
+				}
+
+		}
+
+
+		public static void Main(String[] args)
+		{
+			
+			NeuroNW nw = new NeuroNW(new int[] {2,1 },2);
+			double[] res=nw.Calculate(new double[] { 1, 0.0000000000001 },nw.neuronlist);
+			foreach (double print in res)
+			{
+				if (Math.Abs(print) > 0)
+				{
+					Console.WriteLine(print);
+				}
+			}
+
+		}
+
+		private double[] Calculate(double[] input, neuron[][] neurons)
+		{
+			double[] res = new double[2];
+			double[] layer_answer = new double[2];
+			for (int i = 0; i<neurons.GetLength(0); i++)
+			{
+				for (int n = 0; n < neurons[i].Length;n++)
+				{
+					layer_answer[n] = neurons[i][n].Answer(input);
+				}
+				res = (double[])layer_answer.Clone();
+				input = (double[])layer_answer.Clone();
+				Array.Clear(layer_answer,0,2);
+			}
+			return res;
+		}
+	}
+}
---
+++ neuro/neuron.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace neuro
+{
+	public class neuron
+	{
+		private double _inputs;
+		private double[] _weights;
+		public neuron(int inputs, double[] weights)
+		{
+			if (inputs == weights.Length)
+			{
+				_inputs = inputs;
+				_weights = weights;
+			}
+		}
+		private double summator(double[] signal)
+		{
+			double sum = 0;
+			for (int i = 0; i < _inputs; i++)
+			{
+				sum += signal[i] * _weights[i];
+			}
+			return sum;
+		}
+		public double Answer(double[] signal)
+		{
+			return 2/1+(Math.Pow(Math.E,-1*summator(signal)))-1;
+
+		}
+	}
+}
